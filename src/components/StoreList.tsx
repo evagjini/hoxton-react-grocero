c01c6717c5181617535c01fe11a3735d9a82f1c7
Fevronia Gjini <fevronigjini1@gmail.com>
2022-07-29 14:23:20 +0200

    sm changes

--- src/components/StoreList.tsx
+++ src/components/StoreList.tsx
@@ -1,8 +1,10 @@
+import { CartItem } from "./CartItem"
 import StoreItem from "./StoreItem"
 
 function StoreList() {
     return (
         <ul className="item-list store--item-list">
+            { }
 
             <StoreItem />
         </ul>
